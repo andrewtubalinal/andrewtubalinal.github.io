c6563ff26f009883e346e73059b4e38403139bb7
John Andrew Tubalinal <johnandrew@lapapa88.com>
2024-02-27 16:56:53 +0800

    animation + arc-dialog progress

--- index.js
+++ index.js
@@ -8,37 +8,11 @@ app.config(function($routeProvider) {
         })
 });
 
-app.controller('HomeController', function($scope) {
-    var popupShown = false;
+app.controller('CommonController', function($scope) {
+    $rootScope.displayDialog = function () {
 
-    $scope.showPopup = function() {
-        $scope.isPopupVisible = true;
-        popupShown = true;
-    };
-
-    $scope.hidePopup = function() {
-        $scope.isPopupVisible = false;
-    };
-
-    $scope.addChatBubble = function() {
-        if (!popupShown) {
-            $scope.showPopup();
-        }
-
-        var chatBubble = document.createElement('div');
-        chatBubble.classList.add('chat-bubble');
-        chatBubble.textContent = "Hello!";
-        
-        var arcContainer = document.querySelector('.arc-gif');
-        arcContainer.appendChild(chatBubble);
-        
-        setTimeout(function() {
-            chatBubble.remove();
-        }, 3000);
-    };
-
+    }
 });
-
 
 
 window.addEventListener('load', function() {
@@ -50,7 +24,6 @@ function fadeInElements() {
     const wrapper = document.querySelector('.wrapper');
     const subWrapper = document.querySelector('.sub-wrapper');
     const header = document.querySelector('header');
-    
     body.classList.add('fade-in');
     wrapper.classList.add('fade-in');
     subWrapper.classList.add('fade-in');
