c140308a42afb443ffa8837a434e2e8364729a00
andrewtubalinal <johnandrew@lapapa88.com>
2024-10-10 13:23:46 +0800

    update

--- pages/development.js
+++ pages/development.js
@@ -10,5 +10,24 @@ angular.module('myApp')
             { id: '3', name: 'nodejs', title: 'NodeJS'},
             { id: '4', name: 'angular', title: 'AngularJS'},
             { id: '5', name: 'php', title: 'PHP'},
+            { id: '6', name: 'html', title: 'Hypertext Markup Language'},
+            { id: '7', name: 'css', title: 'Cascading Style Sheet'},
+            { id: '8', name: 'nodejs', title: 'NodeJS'},
+            { id: '9', name: 'angular', title: 'AngularJS'},
+            { id: '10', name: 'php', title: 'PHP'},
         ];
+
+        $scope.slickConfig = {
+            enabled: true,
+            autoplay: true,
+            draggable: false,  
+            autoplaySpeed: 3000,
+            method: {},
+            event: {
+                beforeChange: function (event, slick, currentSlide, nextSlide) {
+                },
+                afterChange: function (event, slick, currentSlide, nextSlide) {
+                }
+            }
+        };
     });
