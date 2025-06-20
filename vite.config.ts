4fa6a60016db8d4d7ae65a7c58482510cb3b9fbf
Andrew <john.tubalinal@gmail.com>
2025-06-21 00:33:28 +0800

    gh-pages clean

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/andrewtubalinal.github.io/', // Add this line!
   build: {
     rollupOptions: {
       output: {
-        // Add content hashing to filenames
         entryFileNames: `assets/[name].[hash].js`,
         chunkFileNames: `assets/[name].[hash].js`,
         assetFileNames: `assets/[name].[hash].[ext]`
       }
     }
-  },
-  base: './' // Critical for GitHub Pages
-});
+  }
+})
